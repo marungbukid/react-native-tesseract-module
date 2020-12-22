2f029978da8a35d359b25fa11760fdeeb4018ded
Jan Maru De Guzman <maru.deguzman@bluefletch.com>
2020-12-22 13:59:18 +0800

    bug-fix, update version

--- android/src/main/java/com/bluefletch/react/TesseractPackage.java
+++ android/src/main/java/com/bluefletch/react/TesseractPackage.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TesseractPackage implements ReactPackage {
